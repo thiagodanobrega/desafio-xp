bcafe2fc7f99d7ccb1a642ee5ad5c88d4a65875f
Thiago Nobrega <thiagons@live.com>
2022-07-16 22:16:43 -0300

    chore: add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import GlobalStyle from "./styles/global";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#1F1F1F",
+        primary: "#121212",
+        secondary: "#F9C732",
+      },
+    },
+  },
+  plugins: [],
+};
