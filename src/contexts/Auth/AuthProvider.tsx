03b68a19a8bdde59ccde40b479a0b5aaa3de6073
Thiago Nobrega <thiagons@live.com>
2022-07-17 12:31:19 -0300

    feat: :sparkles: add provider login function

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-import { AuthContext, IUser } from "./AuthContext";
+import { useApi } from "../../services/auth";
+import { AuthContext, ISignInData, IUser } from "./AuthContext";
 
 export function AuthProvider({ children }: { children: JSX.Element }) {
   const [user, setUser] = useState<IUser | null>(null);
+  const api = useApi();
+
+  // Função que valida usuário e retorna um booleano
+  const signIn = async ({ email, password }: ISignInData): Promise<boolean> => {
+    const { token, user } = await api.sigIn({ email, password });
+    if (token && user) {
+      setUser(user);
+      return true;
+    }
+    return false;
+  };
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, signIn, signout }}>
       {children}
     </AuthContext.Provider>
   );
