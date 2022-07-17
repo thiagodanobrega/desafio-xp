25693cca2078470c36a06ed989b52d1e4c61fb67
Thiago Nobrega <thiagons@live.com>
2022-07-17 14:41:30 -0300

    style: :lipstick: add input style component

---
+++ src/components/Input/style.tsx
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components";
+
+export const InputForm = tw.input`
+  relative 
+  w-full h-12 px-3 py-2 
+  border rounded-md 
+  placeholder-gray-500 text-gray-900 
+  focus:outline-none focus:border-secondary focus:ring-2 focus:ring-offset-2 focus:ring-offset-background focus:ring-secondary sm:text-sm
+`;
