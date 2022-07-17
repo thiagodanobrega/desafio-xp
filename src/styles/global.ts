4151f5b19807bfafef70cf13d465c2204100025a
Thiago Nobrega <thiagons@live.com>
2022-07-16 21:57:15 -0300

    chore: add styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: #1F1F1F;
+    font-size: 16px;
+    color: white;
+    font-family: 'Times New Roman', Times, serif;
+  }
+`;
