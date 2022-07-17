bbca4aaf829f1cec92d32b33d9a7ae5fd7d68585
Thiago Nobrega <thiagons@live.com>
2022-07-17 14:47:06 -0300

    feat: :sparkles: creating login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import LoginForm from "../components/FormLogin";
+
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <div className="bg-background-xp bg-cover bg-center h-screen flex items-center justify-center">
+      <div className="w-[calc(100vw-2rem)] sm:w-[30rem] space-y-8">
+        <div className="mt-10">
+          <img
+            className="mx-auto h-16 w-auto"
+            src="images/logo-xpi.svg"
+            alt="Logo da XP investimentos"
+          />
+          <h1 className="mt-6 text-center text-3xl font-extrabold">
+            Faça login em sua conta
+          </h1>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,11 @@ module.exports = {
         background: "#1F1F1F",
         primary: "#121212",
         secondary: "#F9C732",
+        tertiary: "#202024",
+      },
+      backgroundImage: {
+        "background-xp":
+          "url('https://conteudos.xpi.com.br/wp-content/uploads/2021/07/bg-aprenda-a-investir.png?contrast=-10')",
       },
     },
   },
