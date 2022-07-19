cf5707175fd68276c5c1ec24d938fdcada8f19c3
Thiago Nobrega <thiagons@live.com>
2022-07-19 01:51:06 -0300

    feat(account modal): :sparkles: add withdraw button

--- src/components/AccountModals/TransactionForm.tsx
+++ src/components/AccountModals/TransactionForm.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import Input from "../Input";
+
 interface IAccountForm {
   accountTransaction: (event: { target: HTMLInputElement }) => void;
 }
@@ -19,7 +21,21 @@ function TransactionForm({ accountTransaction }: IAccountForm) {
         >
           Depósito
         </button>
+        <button
+          type="button"
+          onClick={() => setTransactionType("withdraw")}
+          className={`bg-emerald-500 hover:bg-emerald-400 transition-all flex-grow mt-2 flex justify-center items-center py-2 px-4 border border-transparent rounded-md font-bold text-gray-900 focus:outline-none focus:border-emerald-500 focus:ring-2 focus:ring-offset-2 focus:ring-offset-background focus:ring-emerald-500 ${
+            transactionType !== "withdraw" && "opacity-20"
+          } `}
+        >
+          Saque
+        </button>
       </div>
+      <Input
+        type="number"
+        placeholder="Informe o valor"
+        funcEvent={accountTransaction}
+      />
     </div>
   );
 }
