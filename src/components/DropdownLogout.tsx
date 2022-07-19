11e00acd03eb5b815ff7255c4041fbc33a1caf1d
Thiago Nobrega <thiagons@live.com>
2022-07-19 01:29:25 -0300

    feat: :sparkles: cretate logout dropdown

--- src/components/DropdownLogout.tsx
+++ src/components/DropdownLogout.tsx
@@ -1,8 +1,19 @@
 import { Popover } from "@headlessui/react";
 import { CaretDown } from "phosphor-react";
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../contexts/Auth/AuthContext";
 
 function DropdownLogout() {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    auth.signout();
+    navigate("/");
+  };
+
   return (
     <Popover className="relative text-base">
       <Popover.Button
@@ -14,6 +25,18 @@ function DropdownLogout() {
         </p>
         <CaretDown />
       </Popover.Button>
+      <Popover.Panel className="absolute right-0 z-10">
+        <div className="flex flex-col w-16 bg-background rounded-md shadow-lg ring-1 ring-black ring-opacity-5 overflow-auto">
+          <button
+            type="button"
+            className="flex text-center items-center w-full py-2 pl-3 pr-4 transition duration-150 ease-in-out"
+            onClick={logout}
+            title="Botão que faz logout"
+          >
+            Sair
+          </button>
+        </div>
+      </Popover.Panel>
     </Popover>
   );
 }
