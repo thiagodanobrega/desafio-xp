624e974eb35e9f7413ad1f034b32f380c0b841ba
Thiago Nobrega <thiagons@live.com>
2022-07-19 10:50:53 -0300

    chore: removing line space in button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,5 +10,4 @@ export const Button = tw.button`
   font-bold text-gray-900 
   focus:outline-none focus:border-secondary focus:ring-2 focus:ring-offset-2 focus:ring-offset-background focus:ring-secondary
   disabled:opacity-30
-  
 `;
