abd52917e61be0f2151e991c9abd99fed73ed641
Thiago Nobrega <thiagons@live.com>
2022-07-19 10:40:44 -0300

    feat: :sparkles: simulating asset requisition

---
+++ src/services/assetApi.ts
@@ -0,0 +1,80 @@
+// import axios from "axios";
+import { delay } from "./auth";
+
+// const api = axios.create({
+//   baseURL: process.env.REACT_APP_API,
+// });
+
+export const allAssets = [
+  {
+    idAsset: 1,
+    name: "bitcoin",
+    quantity: 5,
+    value: 100,
+  },
+  {
+    idAsset: 2,
+    name: "ethereum",
+    quantity: 4,
+    value: 100,
+  },
+  {
+    idAsset: 3,
+    name: "ripple",
+    quantity: 2,
+    value: 100,
+  },
+  {
+    idAsset: 4,
+    name: "solana",
+    quantity: 3,
+    value: 100,
+  },
+  {
+    idAsset: 5,
+    name: "dogecoin",
+    quantity: 2,
+    value: 100,
+  },
+  {
+    idAsset: 6,
+    name: "tron",
+    quantity: 2,
+    value: 100,
+  },
+];
+
+export const myAssets = [
+  {
+    idAsset: 1,
+    idUser: 1,
+    name: "bitcoin",
+    quantity: 3,
+    value: 100,
+  },
+  {
+    idAsset: 2,
+    idUser: 1,
+    name: "ethereum",
+    quantity: 1,
+    value: 100,
+  },
+];
+
+// simulando requisição a api
+export const assetApi = () => ({
+  getAllAssets: async () => {
+    await delay();
+    return allAssets;
+    // const response = await api.get("/assets");
+    // return response.data;
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getMyAssets: async (userId: any) => {
+    await delay();
+    return myAssets;
+    // const response = await api.get(`/assets/${userId}`);
+    // return response.data;
+  },
+});
