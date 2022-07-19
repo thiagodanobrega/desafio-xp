2fafb8931fe782e31f0fc7d8ca030e8205feabbb
Thiago Nobrega <thiagons@live.com>
2022-07-19 10:48:57 -0300

    feat: :sparkles: providing context of assets in app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { AssetProvider } from "./contexts/Asset/AssetProvider";
 import { AuthProvider } from "./contexts/Auth/AuthProvider";
 import GlobalStyle from "./styles/global";
 import "./styles/global.css";
@@ -11,9 +12,11 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AssetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AssetProvider>
     </AuthProvider>
   </React.StrictMode>
 );
