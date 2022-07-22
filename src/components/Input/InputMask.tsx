4271acba2e21a2edbe34ca55fd8b89b4bbfe9ccd
Thiago Nobrega <thiagons@live.com>
2022-07-22 15:05:55 -0300

    feat: :sparkles: return masked input in component

--- src/components/Input/InputMask.tsx
+++ src/components/Input/InputMask.tsx
@@ -33,6 +33,12 @@ export default function InputMask({
   });
 
   return (
-   
+    <MaskedInput
+      mask={currencyMask}
+      placeholder={placeholder}
+      type={type}
+      onChange={funcEvent}
+      className="relative w-full h-12 px-3 py-2 border rounded-md placeholder-gray-500 text-2xl text-gray-900 focus:outline-none focus:border-secondary focus:ring-2 focus:ring-offset-2 focus:ring-offset-background focus:ring-secondary "
+    />
   );
 }
