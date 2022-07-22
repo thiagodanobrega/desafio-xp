3b9233d041178cefa0dd5beec0b2350c78961de6
Thiago Nobrega <thiagons@live.com>
2022-07-22 14:02:45 -0300

    feat: :sparkles: logic for title render

--- src/components/ComponentsModal/ModalHeader.tsx
+++ src/components/ComponentsModal/ModalHeader.tsx
@@ -1,20 +1,22 @@
+/* eslint-disable react/require-default-props */
 import { Dialog } from "@headlessui/react";
 import { X } from "phosphor-react";
 import React from "react";
 
 interface ICLose {
-  title: string;
+  title?: string;
+  transactionSent?: boolean;
   setIsOppen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function ModalHeader({ setIsOppen, title }: ICLose) {
+function ModalHeader({ setIsOppen, title = "", transactionSent }: ICLose) {
   return (
     <Dialog.Title
       as="h2"
       className="relative flex items-center justify-center text-xl font-bold leading-6 text-light-text
         dark:text-dark-text"
     >
-      {title}
+      {!transactionSent && title}
       <button
         type="button"
         className="absolute -top-1 right-0 rounded-md p-2 text-gray-400 hover:text-light-text hover:bg-zinc-700 dark:hover:text-dark-text transition-all"
