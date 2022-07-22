f6f5aed523baf9a5678bb5518d7ce5308067678d
Thiago Nobrega <thiagons@live.com>
2022-07-22 15:28:45 -0300

    feat: configuring chart

--- src/components/Graphic.tsx
+++ src/components/Graphic.tsx
@@ -45,7 +45,35 @@ function Graphic({ idCrypto }: IGraphic) {
     fetchHistoricPrice();
   }, []);
 
-  return <div />;
+  const data = {
+    labels:
+      historicPrice &&
+      historicPrice.map((price) => {
+        const date = new Date(price[0]);
+        const time = `${date.getHours()}:${date.getMinutes()}`;
+        return days === 1 ? time : date.toLocaleDateString();
+      }),
+
+    datasets: [
+      {
+        label: "Bitcoin",
+        data:
+          historicPrice && historicPrice.map((price) => Math.floor(price[1])),
+        borderColor: "#EEBC1D",
+      },
+    ],
+  };
+
+  const options = {
+    reponsive: true,
+    elements: {
+      point: {
+        radius: 1,
+      },
+    },
+  };
+
+  return <div>{historicPrice && <Line options={options} data={data} />}</div>;
 }
 
 export default Graphic;
