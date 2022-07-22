4eaec7454d490f9f22839590c87b7b48cf0488cf
Thiago Nobrega <thiagons@live.com>
2022-07-22 18:50:36 -0300

    feat: add not found page

--- src/pages/AssetDetails.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AssetDetails() {
-  return <h1>AssetDetails</h1>;
-}
-
-export default AssetDetails;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "../components/Button";
 
 function NotFound() {
-  return <h1>NotFound</h1>;
+  const navigate = useNavigate();
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-background space-y-8 px-4">
+      <img
+        src="/images/not_found.svg"
+        width={500}
+        height={500}
+        alt="Imagem de página não encontrada"
+      />
+      <h1 className="text-xl font-bold text-center">
+        OPPS! PÁGINA NÃO ENCONTRADA
+      </h1>
+      <Button
+        type="button"
+        className="w-[calc(100vw-2rem)] sm:w-60"
+        onClick={() => navigate("/home")}
+      >
+        Voltar para home
+      </Button>
+    </div>
+  );
 }
 
 export default NotFound;
