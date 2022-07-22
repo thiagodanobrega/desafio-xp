7a24f58408e2d4d7a91602816a013003f8027fe7
Thiago Nobrega <thiagons@live.com>
2022-07-22 16:01:23 -0300

    feat: rendering modal in asset tables content

--- src/components/TableAssets/ContentTable/index.tsx
+++ src/components/TableAssets/ContentTable/index.tsx
@@ -1,37 +1,30 @@
-import { Dialog, Transition } from "@headlessui/react";
-import { X } from "phosphor-react";
-import React, { Fragment, useState } from "react";
+import React from "react";
 
-// eslint-disable-next-line import-helpers/order-imports
 import { IUserAssets } from "../../../contexts/Asset/AssetContext";
-
-// eslint-disable-next-line import/named
-import { ContentOfTable } from "./style";
+import AssetModal from "./AssetModal";
 
 interface IAssetProps {
   assets: IUserAssets[];
 }
 
 function ContentTable({ assets }: IAssetProps) {
-  const [isOpen, setIsOppen] = useState(false);
-
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
-      {assets &&
+      {assets.length ? (
         assets.map((asset) => (
-          <ContentOfTable
-            type="button"
-            key={asset.idAsset}
-            onClick={() => setIsOppen(true)}
-          >
-            <span className="text-left">{asset.name}</span>
-            <span className="text-center">
-              {!asset?.idUser ? 1 : asset.quantity}
-            </span>
-            <span className="text-right">{asset.value}</span>
-          </ContentOfTable>
-        ))}
+          <AssetModal
+            asset={asset}
+            key={asset.name}
+            idAsset={asset.idAsset}
+            name={asset.name}
+          />
+        ))
+      ) : (
+        <span className="text-center rounded-md bg-background text-white px-4 py-3">
+          Não há criptoativos
+        </span>
+      )}
     </>
   );
 }
--- src/components/TableAssets/ContentTable/style.tsx
+++ src/components/TableAssets/ContentTable/style.tsx
@@ -1,4 +1,4 @@
 import tw from "tailwind-styled-components";
 
-export const ContentOfTable = tw.button`grid grid-cols-3 items-center rounded-md bg-background hover:bg-zinc-300 hover:text-gray-900 transition-all cursor-pointer px-4 py-4 text-leftbg-secondary rounded-t-md text-sm sm:text-base py-3 text-gray-900 font-bold text-white font-normal
+export const ContentOfTable = tw.button`grid grid-cols-3 items-center rounded-md bg-background hover:bg-zinc-300 hover:text-gray-900 transition-all cursor-pointer px-4 py-3 text-left rounded-t-md text-sm sm:text-base text-gray-900 font-bold text-white font-normal focus:outline-none focus:border-zinc-300 focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-zinc-300
 `;
