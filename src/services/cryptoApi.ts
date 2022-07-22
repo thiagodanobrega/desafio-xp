15d5a605b3dccbd832b8057032f24c5835731920
Thiago Nobrega <thiagons@live.com>
2022-07-22 13:46:18 -0300

    refactor: :recycle: refactoring crypto api request simulation

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -1,15 +1,27 @@
-export const CoinList = (currency: string) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+import axios from "axios";
 
-export const SingleCoin = (id: string) =>
-  `https://api.coingecko.com/api/v3/coins/${id}`;
+export const delay = (amount = 300) =>
+  // eslint-disable-next-line no-promise-executor-return
+  new Promise((resolve) => setTimeout(resolve, amount));
 
-export const HistoricalChart = (
-  id: string,
-  days: number | 365,
-  currency: string
-) =>
-  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+// simulando requisição a api
+export const cryptoApi = () => ({
+  fetchTrendingCoins: async () => {
+    await delay();
+    const { data } = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=BRL&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h"
+    );
 
-export const TrendingCoins = () =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=BRL&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+    return data;
+  },
+
+  fetchHistoricPrices: async (id: string, day: number | 30) => {
+    await delay();
+    const currency = "BRL";
+    const { data } = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${day}`
+    );
+
+    return data.prices;
+  },
+});
