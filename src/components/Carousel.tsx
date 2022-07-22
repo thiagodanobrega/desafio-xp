2c6b3bc9f622b9d6901b99733a248f1915ca8392
Thiago Nobrega <thiagons@live.com>
2022-07-22 13:48:16 -0300

    refactor: :recycle: refactoring carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,58 +1,40 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
-import { TrendingCoins } from "../services/cryptoApi";
+import { CryptoContext } from "../contexts/Crypto/CryptoContext";
 
-interface ITrending {
-  id: string;
-  symbol: string;
-  name: string;
-  image: string;
-  price_change_percentage_24h: number;
-  current_price: number;
-}
 export default function Carousel() {
-  const [trending, setTrending] = useState<ITrending[]>([]);
-
-  const fetchTrendingCoins = async () => {
-    const { data } = await axios.get(TrendingCoins());
-    setTrending(data);
-  };
-
-  useEffect(() => {
-    fetchTrendingCoins();
-  }, []);
-
-  const items = trending.map((coin) => {
-    const profit = coin?.price_change_percentage_24h >= 0;
-
-    return (
-      <div className="flex flex-col items-center text-white text-sm">
-        <img src={coin?.image} alt={coin.name} className="h-8" />
-        <span>
-          {coin?.symbol}
-          &nbsp;
-          <span
-            style={{
-              color: profit ? "rgb(14, 203, 129)" : "red",
-              fontWeight: 500,
-            }}
-          >
-            {profit && "+"}
-            {coin?.price_change_percentage_24h?.toFixed(2)}%
+  const { trendingCoins } = useContext(CryptoContext);
+  const items =
+    trendingCoins &&
+    trendingCoins.map((coin) => {
+      const profit = coin?.price_change_percentage_24h >= 0;
+      return (
+        <div className="flex flex-col items-center text-white text-sm">
+          <img src={coin?.image} alt={coin.name} className="h-8" />
+          <span>
+            {coin?.symbol}
+            &nbsp;
+            <span
+              style={{
+                color: profit ? "rgb(14, 203, 129)" : "red",
+                fontWeight: 500,
+              }}
+            >
+              {profit && "+"}
+              {coin?.price_change_percentage_24h?.toFixed(2)}%
+            </span>
           </span>
-        </span>
-        <span className="text-base font-bold">
-          {coin?.current_price.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          })}
-        </span>
-      </div>
-    );
-  });
+          <span className="text-base font-bold">
+            {coin?.current_price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </span>
+        </div>
+      );
+    });
 
   const responsive = {
     0: {
@@ -65,17 +47,19 @@ export default function Carousel() {
 
   return (
     <div className="flex w-[calc(100vw-2rem)] md:w-[50rem] absolute -bottom-9 items-center h-2/4">
-      <AliceCarousel
-        mouseTracking
-        infinite
-        autoPlayInterval={1000}
-        animationDuration={1500}
-        disableDotsControls
-        disableButtonsControls
-        responsive={responsive}
-        items={items}
-        autoPlay
-      />
+      {trendingCoins && (
+        <AliceCarousel
+          mouseTracking
+          infinite
+          autoPlayInterval={1000}
+          animationDuration={1500}
+          disableDotsControls
+          disableButtonsControls
+          responsive={responsive}
+          items={!items ? [] : items}
+          autoPlay
+        />
+      )}
     </div>
   );
 }
