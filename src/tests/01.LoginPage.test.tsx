b3e0aeb1bc05b5ab9fdeeb50a8c7e5d2f2552988
Thiago Nobrega <thiagons@live.com>
2022-07-24 11:44:40 -0300

    test: login: testando o titulo da pagina

---
+++ src/tests/01.LoginPage.test.tsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { screen, fireEvent } from "@testing-library/react";
+import React from "react";
+import { describe, expect, test } from "vitest";
+
+import Login from "../pages/Login";
+import renderWithRouter from "../utils/renderWithRouter";
+
+const EMAIL_INPUT_TEST_ID = "input-email";
+const PASSWORD_INPUT_TEST_ID = "input-password";
+const BUTTON_LOGIN_TEST_ID = "btn-login";
+const VALID_EMAIL = "test@email.com";
+const VALID_PASSWORD = "1234567";
+const INVALID_EMAIL = "alguem@email.";
+const INVALID_PASSWORD = "23456";
+
+describe("1 - Testando a tela de login", () => {
+  test("Verifica se possui o título 'Faça login em sua conta'", () => {
+    renderWithRouter(<Login />);
+    const title = screen.getByText(/Faça login em sua conta/i);
+    expect(title).toBeInTheDocument();
+  });
+});
+export {};
