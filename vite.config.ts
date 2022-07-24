07c02cbb428ab83c5e40e12fc0f86ff3d0b20bc2
Thiago Nobrega <thiagons@live.com>
2022-07-23 22:54:17 -0300

    chore:

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["**/(*.)?{test,spec}.{js,ts,jsx,tsx}"],
+    exclude: ["node_modules"],
+    setupFiles: "./src/tests/setup.ts",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
+});
